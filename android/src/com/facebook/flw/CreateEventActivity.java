659b3eba0d1cd59811294a57a30dd66085140b78
Natthu Bharambe <natthu@fb.com>
2013-09-27 01:32:58 +0100

    CreateEvent
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- android/src/com/facebook/flw/CreateEventActivity.java
+++ android/src/com/facebook/flw/CreateEventActivity.java
@@ -1,9 +1,15 @@
 package com.facebook.flw;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import com.facebook.flw.model.Event;
 import com.facebook.flw.model.Restaurant;
@@ -28,11 +34,30 @@ public class CreateEventActivity extends Activity {
         setContentView(R.layout.create_event);
       setTitle("Choose Restaurants");
 
-        ListView list = (ListView)findViewById(R.id.rest_list);
+        final ListView list = (ListView)findViewById(R.id.rest_list);
+        final ListAdapter adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_multiple_choice, restaurants);
         list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        list.setAdapter(new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_multiple_choice, restaurants));
+        list.setAdapter(adapter);
 
+        Button submit = (Button)findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SparseBooleanArray checked = list.getCheckedItemPositions();
+                ArrayList<String> selectedNames = new ArrayList<String>();
+                for (int i = 0; i < adapter.getCount(); ++i) {
+                    if (checked.get(i)) {
+                        selectedNames.add((String)adapter.getItem(i));
+                    }
+                }
+
+                Log.d("natthu", "checked: " + selectedNames);
+            }
+        });
+
+
+        final ProgressDialog dialog = ProgressDialog.show(this, "", "Searching restaurants nearby ...", true);
     }
 
   private void createEventWithRestaurants() {
